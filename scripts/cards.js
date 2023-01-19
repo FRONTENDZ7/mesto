ba92df9620be553282b8cf48eb979e0f955a30de
burondt <denisalexandrovich@inbox.ru>
2023-01-20 02:14:22 +0300

    Update cards.js

--- scripts/cards.js
+++ scripts/cards.js
@@ -1,26 +1,27 @@
 const initialCards = [
     {
+      name: 'Сочи',
+      link: 'https://kidpassage.com/images/publications/sochi-yanvare-otdyh-pogoda/sochi-yanvare-otdyh-pogoda-photo1_1474496836.jpg'
+    },
+    {
       name: 'Роза хутор',
       link: 'https://images.unsplash.com/photo-1601035821612-8cb0ffba4083?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80'
     },
     {
-      name: 'Архыз',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+      name: 'Овьедо',
+      link: 'https://www.paismagico.travel/wp-content/uploads/2016/12/Oviedo-1080x675.jpg'
     },
     {
-      name: 'Челябинская область',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+      name: 'Санкт-Петербург',
+      link: 'https://about-planet.ru/images/evropa/goroda/sankt-peterburg/sankt-peterburg10.jpg'
     },
     {
-      name: 'Камчатка',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
-    },
-    {
-      name: 'Холмогорский район',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+      name: 'Москва',
+      link: 'https://planetofhotels.com/guide/sites/default/files/styles/paragraph__text_with_image___twi_image/public/2021-09/Presnensky.jpg'
     },
     {
       name: 'Байкал',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+      link: 'https://tripplanet.ru/wp-content/uploads/europe/russia/baikal/dostoprimechatelnosti-bajkala.jpg'
     }
   ];
+ 
